1ba53a6fd0cb75bf8032bf6ec986080a5dc122fb
Taisa <taisa@MacBook-Pro-de-Taisa-2.local>
2018-11-12 10:27:50 -0300

    rm test

--- test.py
+++
@@ -1,5 +0,0 @@
-superlist = ['/portal/productos-persona/seguros-vehiculos/seguro-auto-flexible','/portal/productos-persona/seguros-vehiculos/seguroxkm','/portal/productos-persona/seguros-de-vida-y-accidentes/enfermedades-protegidas']
-
-for item in superlist:
-    split = item.split('/')
-    print(split[-1])
